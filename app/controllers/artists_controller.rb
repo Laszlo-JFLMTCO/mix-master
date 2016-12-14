4f7be7987ec88b66be45439b2fcd43cddc4dc97a
Laszlo Balogh <laszlo@thebaloghs.com>
2016-12-13 17:55:34 -0700

    All routes functional, basic bootstrap included

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -10,11 +10,37 @@ class ArtistsController < ApplicationController
 
   def create
     @artist = Artist.create(artist_params)
-    redirect_to artist_path(@artist.name)
+    if @artist.save
+      redirect_to artist_path(@artist.name)
+    else
+      @errors = @artist.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    @artist = Artist.find_by(name: params[:name])
+  end
+
+  def update
+    @artist = Artist.find_by(name: params[:name])
+    @artist.update(params[:artist])
+    if @artist.save
+      redirect_to artist_path(@artist.name)
+    else
+      @errors = @artist.errors.full_messages
+      render :edit
+    end
   end
 
   def show
     @artist = Artist.find_by(name: params[:name])
+  end
+
+  def destroy
+    artist = Artist.find_by(name: params[:name])
+    Artist.delete(artist)
+    redirect_to artists_path
   end
 
   private
